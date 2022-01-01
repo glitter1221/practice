32ca880a77ddcf2b694773f0ccf35f498c1b6fdb
glitter1221 <sinscargh7@naver.com>
2022-01-01 23:06:50 +0900

    zoom day 3

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -3,6 +3,12 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT | 3000;
 
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
 app.listen(port).then(() => console.log(`server connected on ${port}!`));
 
 console.log('hell0!');
