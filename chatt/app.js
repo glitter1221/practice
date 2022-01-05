2147f342e5c1fe3638e83840d3ef04ab13080c52
glitter1221 <sinscargh7@naver.com>
2022-01-05 23:18:38 +0900

    socket chat app day 1

---
+++ chatt/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const http = require('http');
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+
+const socketIO = require('socket.io');
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname, 'src')));
+server.listen(port, () => { 
+    console.log(`Connected on ${port}!`)
+});
+
+console.log(path.join(__dirname, 'src'));
+
+io.on("connection", (socket) => {
+    console.log('socket connected!');
+});
--- zoom/README.md
+++
@@ -1,7 +0,0 @@
-# video chatting
-
-## from : https://nomadcoders.co/noom/lobby
-
-### its video chatting with socket.io and webRTC on Websockets
-
-### or just chat websocket
