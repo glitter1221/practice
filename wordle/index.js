309e01e18f2be56d86cefe357d4268dcba540289
glitter1221 <sinscargh7@naver.com>
2022-05-27 11:51:33 +0900

    correct count

--- wordle/index.js
+++ wordle/index.js
@@ -4,15 +4,30 @@ document.querySelector('button').addEventListener('click', () => {
     let input = document.querySelectorAll('.input');
 
     for(let i = 0; i < 5; i++) {
-        if(input[0].value == answer[0]) {
-            input[0].style.background = 'green';
-        } else if (answer.includes(input[0].value)) {
-            input[0].style.background = 'yellow';
+        if(input[i].value == answer[i]) {
+            input[i].style.background = 'green';
+        } else if (answer.includes(input[i].value)) {
+            input[i].style.background = 'yellow';
         } else {
-            input[0].style.background = 'lightgrey';
+            input[i].style.background = 'lightgrey';
         }
 
         input[i].classList.remove('input');
+
+        // 다 맞으면 정답 공개하기
+
+        let correctCount;
+
+        for(j=0; j<5; j++) {
+            if(input[j].style.background == 'green') {
+                correctCount++;
+            }
+        }
+        if(correctCount != 5) {
+            correctCount = 0;
+        } else if (correctCount == 5) {
+            // 정답 화면 어떻게 만들지?
+        }
     }
 
     let template = `<div>
@@ -22,6 +37,7 @@ document.querySelector('button').addEventListener('click', () => {
         <input class="input">
         <input class="input">
     </div>`;
+
     document.querySelector('body').insertAdjacentHTML('beforeend', template);
 
     // 위치까지 맞으면 green box
