b1b0bf80d217ecbd5e4e48f7a7bcb2e81265cb18
glitter1221 <sinscargh7@naver.com>
2022-06-02 23:30:12 +0900

    retry butn.

--- wordle/index.js
+++ wordle/index.js
@@ -1,45 +1,29 @@
 let answer = 'abcde';
 
+let template = `
+    <input type="text" class="input">
+    <input type="text" class="input">
+    <input type="text" class="input">
+    <input type="text" class="input">
+    <input type="text" class="input">
+`;
+
 document.querySelector('button').addEventListener('click', () => {
     
     let input = document.querySelectorAll('.input');
 
     for(let i = 0; i < 5; i++) {
-        if(input[i].value == answer[i]) {
+        if(input[i].value === answer[i]) {
             input[i].style.background = 'green';
         } else if (answer.includes(input[i].value)) {
             input[i].style.background = 'yellow';
         } else {
             input[i].style.background = 'red';
         }
-
-        input[i].classList.remove('input');
-
-
-        // 정답 기회 5번 기능?
-        let correctCount;
-
-        for(j=0; j<5; j++) {
-            if(input[j].style.background == 'green') {
-                correctCount++;
-            }
-        }
-        if(correctCount != 5) {
-            correctCount = 0;
-        } else if (correctCount == 5) {
-            // 정답 화면 어떻게 만들지?
-        }
+        // input[i].classList.remove('input');
     }
 
-    let template = `
-        <input type="text" class="input">
-        <input type="text" class="input">
-        <input type="text" class="input">
-        <input type="text" class="input">
-        <input type="text" class="input">
-    `;
-
-    document.querySelector('body').insertAdjacentHTML('beforeend', template);
+    document.querySelector('contents').insertAdjacentHTML('beforeend', template);
 
     // 위치까지 맞으면 green box
     // 위치는 틀리면 yellow box
